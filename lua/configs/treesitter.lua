f4753bf3207299efbae649ef6e5c18a5f9f46306
lijinglin <me@lijinglin.dev>
2022-07-08 18:53:03 +0800

    fix fold problem with telescope

--- lua/configs/treesitter.lua
+++ lua/configs/treesitter.lua
@@ -8,3 +8,13 @@ require('nvim-treesitter.configs').setup({
 
 vim.opt.foldmethod = 'expr'
 vim.opt.foldexpr = 'nvim_treesitter#foldexpr()'
+
+-- https://github.com/nvim-telescope/telescope.nvim/issues/559
+vim.api.nvim_create_autocmd('BufRead', {
+	callback = function()
+		vim.api.nvim_create_autocmd('BufWinEnter', {
+			once = true,
+			command = 'normal! zx'
+		})
+	end
+})
