e77da9b9308ab54313112561e3f516b5aa0e3dfe
lijinglin <me@lijinglin.dev>
2022-07-23 03:19:28 +0800

    update gitsigns configs

--- lua/configs/gitsigns.lua
+++ lua/configs/gitsigns.lua
@@ -1,1 +1,9 @@
-require('gitsigns').setup()
+require('gitsigns').setup({
+	numhl = true,
+	word_diff = true,
+	current_line_blame = true,
+	current_line_blame_opts = {
+		delay = 200,
+		virt_text_pos = 'right_align'
+	}
+})
