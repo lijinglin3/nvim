bc0d099555c44ad8de3cf71a070e1a608aa54c91
lijinglin <me@lijinglin.dev>
2022-08-19 17:21:48 +0800

    fix lualine

--- lua/configs/lualine.lua
+++ lua/configs/lualine.lua
@@ -2,7 +2,7 @@ require('lualine').setup({
 	sections = {
 		lualine_a = { 'mode' },
 		lualine_b = { 'branch', 'diff', 'diagnostics' },
-		lualine_c = { { 'filename', path = 3 }, 'aerial' },
+		lualine_c = { { 'filename', path = 3 } },
 		lualine_x = { 'encoding', 'fileformat', 'filetype' },
 		lualine_y = { 'progress' },
 		lualine_z = { 'location' }
