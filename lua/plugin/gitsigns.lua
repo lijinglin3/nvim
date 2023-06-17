e37dc25202b1376d22e1c69752b69fdd090e8784
lijinglin <me@lijinglin.dev>
2023-06-18 00:29:43 +0800

    update gitsigns

--- lua/plugin/gitsigns.lua
+++ lua/plugin/gitsigns.lua
@@ -2,9 +2,7 @@ local gs = require('gitsigns')
 
 gs.setup({
     numhl = true,
-    yadm = {
-        enable = true
-    },
+    yadm = { enable = true },
     on_attach = function(bufnr)
         local opts = { buffer = bufnr, noremap = true }
         vim.keymap.set('n', '<leader>hb', function() gs.blame_line { full = true } end, opts)
@@ -17,14 +15,14 @@ gs.setup({
         vim.keymap.set('n', '<leader>hd', gs.diffthis, opts)
 
         opts.expr = true
-        vim.keymap.set('n', ']]', function()
-            if vim.wo.diff then return ']]' end
+        vim.keymap.set('n', '}}', function()
+            if vim.wo.diff then return '}}' end
             vim.schedule(function() gs.next_hunk() end)
             return '<Ignore>'
         end, opts)
 
-        vim.keymap.set('n', '[[', function()
-            if vim.wo.diff then return '[[' end
+        vim.keymap.set('n', '{{', function()
+            if vim.wo.diff then return '{{' end
             vim.schedule(function() gs.prev_hunk() end)
             return '<Ignore>'
         end, opts)
