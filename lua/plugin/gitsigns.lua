38b113c4fa0e90fe83df1987f777b484b4e7382a
lijinglin <me@lijinglin.dev>
2023-06-18 00:23:39 +0800

    update gitsigns

--- lua/plugin/gitsigns.lua
+++ lua/plugin/gitsigns.lua
@@ -2,30 +2,31 @@ local gs = require('gitsigns')
 
 gs.setup({
     numhl = true,
+    yadm = {
+        enable = true
+    },
     on_attach = function(bufnr)
         local opts = { buffer = bufnr, noremap = true }
-        vim.keymap.set({ 'n', 'v' }, '<leader>hs', gs.stage_hunk, opts)
-        vim.keymap.set({ 'n', 'v' }, '<leader>hr', gs.reset_hunk, opts)
-        vim.keymap.set('n', '<leader>hS', gs.stage_buffer, opts)
-        vim.keymap.set('n', '<leader>hR', gs.reset_buffer, opts)
+        vim.keymap.set('n', '<leader>hb', function() gs.blame_line { full = true } end, opts)
+        vim.keymap.set('v', '<leader>hs', function() gs.stage_hunk { vim.fn.line('.'), vim.fn.line('v') } end, opts)
+        vim.keymap.set('v', '<leader>hr', function() gs.reset_hunk { vim.fn.line('.'), vim.fn.line('v') } end, opts)
+        vim.keymap.set('n', '<leader>hs', gs.stage_hunk, opts)
+        vim.keymap.set('n', '<leader>hr', gs.reset_hunk, opts)
         vim.keymap.set('n', '<leader>hu', gs.undo_stage_hunk, opts)
         vim.keymap.set('n', '<leader>hp', gs.preview_hunk, opts)
         vim.keymap.set('n', '<leader>hd', gs.diffthis, opts)
-        vim.keymap.set('n', '<leader>hD', gs.toggle_deleted, opts)
-        vim.keymap.set('n', '<leader>hb', gs.toggle_current_line_blame, opts)
-        vim.keymap.set('n', '<leader>hB', function() gs.blame_line { full = true } end, opts)
 
         opts.expr = true
         vim.keymap.set('n', ']]', function()
             if vim.wo.diff then return ']]' end
             vim.schedule(function() gs.next_hunk() end)
-            return '<ignore>'
+            return '<Ignore>'
         end, opts)
 
         vim.keymap.set('n', '[[', function()
             if vim.wo.diff then return '[[' end
             vim.schedule(function() gs.prev_hunk() end)
-            return '<ignore>'
+            return '<Ignore>'
         end, opts)
     end
 })
